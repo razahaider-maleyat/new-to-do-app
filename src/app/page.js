c860db864668643adbe4cae20018cfdb27ecbe68
razahaiderjalip <razahaiderjalip@gmail.com>
2024-12-04 12:07:20 +0500

    task done

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,111 @@
-import Image from "next/image";
+"use client";
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import { Box, TextField } from "@mui/material";
 
-export default function Home() {
+export default function BasicTable() {
+  const [editName, setEditName] = React.useState("");
+  const [secupdateColum, setSecUpdateColum] = React.useState("");
+  const [updateColum, setUpdateColum] = React.useState([]);
+  const [editMode, setEditMode] = React.useState(null);
+
+  const updateName = (e) => {
+    e.preventDefault();
+    if (editMode) {
+      const updatedColumns = updateColum.map((item) =>
+        item.id === editMode ? { ...item, editName, secupdateColum } : item
+      );
+      setUpdateColum(updatedColumns);
+      setEditMode(null);
+    } else {
+      const newTask = { id: Date.now(), editName, secupdateColum };
+      setUpdateColum([...updateColum, newTask]);
+    }
+    setEditName("");
+    setSecUpdateColum("");
+  };
+
+  const deleHandler = (id) => {
+    let deletColum = [...updateColum];
+    deletColum = deletColum.filter((item) => item.id !== id);
+    setUpdateColum(deletColum);
+  };
+
+  const handleEdit = (id) => {
+    const taskToEdit = updateColum.find((item) => item.id === id);
+    setEditName(taskToEdit.editName);
+    setSecUpdateColum(taskToEdit.secupdateColum);
+    setEditMode(id);
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <TableContainer component={Paper} sx={{ width: 850, margin: "auto" }}>
+      <Table aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Users Name</TableCell>
+            <TableCell>
+              <Box
+                component="form"
+                sx={{ "& > :not(style)": { m: 1, width: "150px" } }}
+                noValidate
+                autoComplete="off"
+                onSubmit={updateName}
+                display={"flex"}
+              >
+                <TextField
+                  id="standard-basic"
+                  label="First Name"
+                  variant="standard"
+                  value={secupdateColum}
+                  onChange={(e) => setSecUpdateColum(e.target.value)}
+                />
+                <TextField
+                  id="standard-basic"
+                  label="Last Name"
+                  variant="standard"
+                  value={editName}
+                  onChange={(e) => setEditName(e.target.value)}
+                />
+                <button type="submit">
+                  {editMode ? "Update Data" : "Add Data"}
+                </button>
+              </Box>
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {updateColum.map((t) => (
+            <TableRow
+              key={t.id}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                <h5>{t.editName}</h5>
+                <h5>{t.secupdateColum}</h5>
+                <button
+                  onClick={() => {
+                    deleHandler(t.id);
+                  }}
+                >
+                  <DeleteIcon />
+                </button>
+                <button onClick={() => handleEdit(t.id)}>
+                  <EditIcon />
+                </button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
